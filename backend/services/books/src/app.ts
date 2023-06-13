7d11b868d63de4014ae7fdf0220362e64c55395c
Gabriel Brzoskwinia <gabriel.brzoskwinia@sabre.com>
2023-06-13 18:06:29 +0200

    defining routes and setting up server

---
+++ backend/services/books/src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application } from 'express'
+import mongoose, { ConnectOptions } from 'mongoose'
+import bookRoutes from './routes/bookRoutes'
+
+const app: Application = express()
+const PORT = 3001
+const CONNECTION_STRING = 'mongodb://localhost:27017/bookstore'
+
+app.use(express.json())
+
+mongoose.connect(CONNECTION_STRING)
+.then(() => {
+    console.log('Successfully connected to mongoDB')
+})
+.catch(error => {
+    console.log('MongoDB connection error: ', error)
+})
+
+app.use('/', bookRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
