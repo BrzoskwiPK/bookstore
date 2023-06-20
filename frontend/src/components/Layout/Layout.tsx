cf74a4fcb3c89c757dcd5b8b58d50f8f36eb30ed
Brzoskwi <brzoskwinia.gabriel@gmail.com>
2023-06-20 21:24:49 +0200

    reverting changes

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -3,18 +3,14 @@ import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
 import { FC } from 'react'
 import { containerStyle, outletStyle } from './Styles'
-import Login from '../../pages/Login/Login'
 
-interface LayoutProps {
-  isLoggedIn: boolean
-}
 
-const Layout: FC<LayoutProps> = ({ isLoggedIn }) => {
+const Layout: FC = () => {
   return (
     <div style={containerStyle}>
       <Header />
       <div style={outletStyle}>
-        {isLoggedIn ? <Outlet /> : <Login />}
+        <Outlet />
       </div>
       <Footer />
     </div>
